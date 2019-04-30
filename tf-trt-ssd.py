570463c4992ab37de62846af67709a3247d25f47
Tsutomu Furuse <47056802+tsutof@users.noreply.github.com>
2019-04-30 23:06:27 +0900

    Update tf-trt-ssd.py

--- tf-trt-ssd.py
+++ tf-trt-ssd.py
@@ -1,6 +1,6 @@
 # MIT License
 #
-# Copyright (c) 2019 tsutof
+# Copyright (c) 2019 Tsutomu Furuse
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
